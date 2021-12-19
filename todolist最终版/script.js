893bbb74cbf81608cf1b40d26263c4bf8ed02b6a
sakliuwj <89829085+sakliuwj@users.noreply.github.com>
2021-12-19 14:16:48 +0800

    Add files via upload

---
+++ todolist最终版/script.js
@@ -0,0 +1,170 @@
+
+
+
+let todoItems = [];
+let finishedItems = [];
+
+function renderTodoItemList(todoItems, finishedItems) {
+
+    let paneEl = document.querySelector("#todolist > .list-pane");
+    paneEl.innerHTML = "";
+
+    for (let i=0; i < todoItems.length; i++ ) {
+        let item = todoItems[i];
+        let itemDiv = document.createElement("div");
+        itemDiv.className = "todo-item";
+        //勾选完成
+        let inputEl = document.createElement("input");
+        inputEl.type = "checkbox";
+
+        inputEl.addEventListener("change", (e) => {
+            finishedItems.push(item);
+            todoItems.splice(i, 1);
+            //这两行好像没用，但是我不敢注释掉。。。
+            console.log("finshed:", i, todoItems, finishedItems );
+            renderTodoItemList(todoItems, finishedItems);
+        });
+        //事项内容
+        let titleEl = document.createElement("div");
+        titleEl.className = "title";
+        //修改表内事项
+        titleEl.contentEditable="true" ;
+        titleEl.addEventListener("mousedown",(e)=>{
+            let mark=0//如果mark=0说明没有被执行过下面的函数，也就是用户还没改动。
+            let originEl =todoItems[i].title
+            titleEl.addEventListener("input",(e)=>{
+                if(mark==0){ 
+                  titleEl.addEventListener("mouseleave",(e)=>{
+                      if(originEl!=titleEl.innerText){ //不相等说明用户改动了。
+                         if(mark==0 && confirm("确认修改？")){
+                                todoItems[i].title = titleEl.innerText;
+                                mark=1;  //说明函数confirm被执行过，防止一直询问
+                                renderTodoItemList(todoItems, finishedItems);
+            
+                                originEl=titleEl.innerText
+                            }else{
+                                todoItems[i].title=originEl; 
+                                renderTodoItemList(todoItems, finishedItems); //重新恢复不修改的样子
+                                mark=1;
+                            }
+                        }
+                    })
+                }
+            })   
+        })
+
+        //设置重要性
+        let importanceEl = document.createElement("div");
+        importanceEl.className = "important-flag"
+        importanceEl.innerText = "!";
+
+        if (item.isImportance) {
+            importanceEl.classList.add("open");
+            itemDiv.classList.add("important");
+        }
+
+        importanceEl.addEventListener("click", (e) => {
+            console.log("click: ", item);
+            if (item.isImportance) {
+                item.isImportance = false;
+            } else {
+                item.isImportance = true;
+            }
+
+            renderTodoItemList(todoItems, finishedItems);
+        });
+        //删除事项键
+        let deleteBtn = document.createElement("button");
+        deleteBtn.className="deletebtn"
+        deleteBtn.innerText = "X";
+
+        deleteBtn.addEventListener("click", (e) => {
+            if (confirm("确认删除?")){
+                todoItems.splice(i,1)
+                deleteBtn.parentNode.remove()
+            }
+        });
+         
+
+
+        titleEl.innerText = item.title;
+
+        itemDiv.append(inputEl);
+        itemDiv.append(titleEl);
+        itemDiv.append(importanceEl);
+        itemDiv.append(deleteBtn);
+        
+        paneEl.append(itemDiv);
+    }
+
+}
+
+
+
+function renderFinishedItemList(todoItems, finishedItems) {
+
+    let paneEl = document.querySelector("#todolist > .list-pane");
+    paneEl.innerHTML = "";
+
+    for (let i=0; i < finishedItems.length; i++ ) {
+        let item = finishedItems[i];
+        let itemDiv = document.createElement("div");
+        itemDiv.className = "todo-item";
+
+
+        let titleEl = document.createElement("div");
+        titleEl.className = "title";
+
+        let importanceEl = document.createElement("div");
+        importanceEl.className = "important-flag"
+        importanceEl.innerText = "!";
+        if (item.isImportance) {
+            importanceEl.classList.add("open");
+        }
+        
+
+        titleEl.innerText = item.title;
+
+        itemDiv.append(titleEl);
+        itemDiv.append(importanceEl);
+        
+        paneEl.append(itemDiv);
+    }
+
+}
+
+
+function renderInputPane(todoItems) {
+    let inputPaneEl = document.querySelector("#todolist > .input-pane");
+
+    let addBtnEl = inputPaneEl.querySelector("#add-btn");
+    let hisBtnEl = inputPaneEl.querySelector("#his-btn");
+
+    addBtnEl.addEventListener("click", (e)=>{
+        let inputEl = inputPaneEl.querySelector("input");
+        if(inputEl.value==""){
+          alert("空白事项")
+        }else{
+          todoItems.push({
+          title: inputEl.value,
+          isFinished: false,
+          isImportance: false, 
+         })
+          inputEl.value=""
+        }
+        renderTodoItemList(todoItems, finishedItems);
+    });
+
+    hisBtnEl.addEventListener("click", (e)=>{
+        if (hisBtnEl.classList.contains("open")) {
+            hisBtnEl.classList.remove("open");
+            renderTodoItemList(todoItems, finishedItems)//这行也是测试的吧可能(x)运行一遍生成结果
+        } else {
+            hisBtnEl.classList.add("open");
+            renderFinishedItemList(todoItems, finishedItems)
+        }
+    });
+}
+
+renderInputPane(todoItems, finishedItems);
+renderTodoItemList(todoItems, finishedItems);
